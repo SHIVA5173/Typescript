c3831b02c5608cb9d8b2872de0c0dfb2a05d682e
Shiva WebDev <psivaiah2003@gmail.com>
2025-01-23 22:01:03 +0530

    Tuples In Ts

---
+++ Tuples.ts
@@ -0,0 +1,13 @@
+// Tuples: Arrays with fixed numbers of elements.
+
+let Emp: [number, string, string, number] = [
+  4881,
+  "Shiva WebDev",
+  "shiva@gmail.com",
+  658000,
+];
+
+console.log(Emp);
+
+let Products: (number | string)[] = [4881, "iphone 18pro max", "$256k"];
+console.log(Products);

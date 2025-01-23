181207f64ed3e09cdcc59981d35c78b9b57b8b6a
Shiva WebDev <psivaiah2003@gmail.com>
2025-01-23 20:50:05 +0530

    Literal Types In Ts

---
+++ LiteralTypes.ts
@@ -0,0 +1,13 @@
+let color: "Red" | "Crimson" | "Teal";
+color = "Crimson"; // valid
+// color = "Yellow"; //invalid
+
+console.log(color);
+
+let Scores: 100 | 200 | 300;
+Scores = 200; // valid
+// Scores = 1000; //invalid
+console.log(Scores);
+
+let isUser: true | false;
+isUser = true;
